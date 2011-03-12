d75382a111fa9ccec0e40bcd219b8aeef97e73d9
Tatham Oddie <tatham@oddie.com.au>
2011-03-12 16:54:39 +1100

    Added TimeSpan extension method.

---
+++ RelativeTime/TimeSpanExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RelativeTime
+{
+    public static class TimeSpanExtensions
+    {
+        static readonly Formatter formatter = new Formatter();
+
+        public static string ToHumanString(this TimeSpan timeSpan)
+        {
+            return formatter.Format(timeSpan);
+        }
+    }
+}
