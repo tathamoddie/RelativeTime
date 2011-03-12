18635e111aeac5aa6db8433aa272e1de90918273
Tatham Oddie <tatham@oddie.com.au>
2011-03-12 16:31:03 +1100

    Added project structure.

---
+++ RelativeTime/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Relative Time")]
+[assembly: AssemblyCompany("Tatham Oddie")]
+[assembly: AssemblyProduct("Relative Time")]
+[assembly: AssemblyCopyright("Copyright © Tatham Oddie 2011 MS-PL")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70811a55-e6ee-49f2-a860-55e3bc0ead3f")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
